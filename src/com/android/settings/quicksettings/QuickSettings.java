kasperhettinga/android_packages_apps_Settings

QS: Make it possible to show smaller icons (2/2)
Change-Id: I974a8e17dfc5ae6b62af1ec87129c6f04508a865

src/com/android/settings/cyanogenmod/QuickSettings.java
@@ -52,6 +52,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String GENERAL_SETTINGS = "pref_general_settings";
     private static final String STATIC_TILES = "static_tiles";
     private static final String DYNAMIC_TILES = "pref_dynamic_tiles";
+    private static final String SHOW_LABELS = "qs_show_text";
 
     private MultiSelectListPreference mRingMode;
     private ListPreference mNetworkMode;

darklordIN/platform_packages_apps_Settings

src/com/android/settings/quicksettings/QuickSettings.java
@@ -54,6 +54,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String GENERAL_SETTINGS = "pref_general_settings";
     private static final String STATIC_TILES = "static_tiles";
     private static final String DYNAMIC_TILES = "pref_dynamic_tiles";
+    private static final String HIDE_LABELS = "qs_hide_text";
 
     private MultiSelectListPreference mRingMode;
     private ListPreference mNetworkMode;