8723955094a41ec5fc734d750dbb1b51688fe0d3
Arnav Gosain <nolinuxnoparty@gmail.com>
2014-01-27 13:57:55 -0500

    Settings: Remove RamBar
    Signed-off-by: Arnav Gosain <nolinuxnoparty@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1218,6 +1218,5 @@ public class Settings extends PreferenceActivity
     public static class ApnEditorActivity extends Settings { /* empty */ }
     public static class BlacklistSettingsActivity extends Settings { /* empty */ }
     public static class SystemSettingsActivity extends Settings { /* empty */ }
-    public static class ASSRamBarActivity extends Settings { /* empty */ }
     public static class AboutActivity extends Settings { /* empty */ }
 }
